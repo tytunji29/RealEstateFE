24108442473296ec90a5f5351f30eee413efe26b
tunji ty <tytunji29@gmail.com>
2025-08-27 15:17:11 +0100

    vggg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-// import { redirect } from 'next/navigation';
-
-// export default function Page(): never {
-//   redirect('/dashboard');
-// }
-
-
-'use client';
-
-import { Providers } from '@/app/providers';
-
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  return <Providers>{children}</Providers>;
+export default function Page() {
+  return (
+    <div>
+      <h1>Welcome to Home Page</h1>
+    </div>
+  );
 }
